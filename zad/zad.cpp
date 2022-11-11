a724b8a723ed9675845fcc52a268ee31e762641f
306 <306@306A-22>
2022-11-11 12:48:16 +0300

    Добавьте файлы проекта.

---
+++ zad/zad.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int Number;
+    int Choice;
+    
+    cout << "Введите размер массива:" << " ";
+    cin >> Number;
+
+    int *Arr = new int[Number];
+
+    srand(time(NULL));
+        for (int i = 0; i < Number; i++) {
+            Arr[i] = rand();
+                cout << "|" << Arr[i] << "| ";
+        }
+
+    cout << endl;
+    cout << "Введите элемент массива (элементы массива начинаются с 0!), который хоитите удалить:" << " ";
+    cin >> Choice;
+
+        for (long i = Choice; i < Number - 1; ++i) {
+                Arr[i] = Arr[i + 1];
+        }
+     --Number;
+
+        for (int i = 0; i < Number; i++) {
+                cout << "|" << Arr[i] << "| ";
+        }
+
+    delete[] Arr;
+    return 0;
+}
